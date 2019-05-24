6f19069146753b4418dc3a47977b9fca22ca3776
solecardazzo <soledadcardazzo@gmail.com>
2019-05-24 15:00:55 -0300

    cambio a php e include header

---
+++ agradecimientos.php
@@ -0,0 +1,90 @@
+<?php
+$pageTitle = "Agradecimientos";
+require("header.php");
+?>
+
+  <section class="agradecimientos">
+    <div class="titulos-agradecimientos">
+      <h2 class="title-agrade"> Agradecimientos </h2>
+      <p>
+        Las siguientes empresas colaboran con la Fundación.
+      </p>
+      <p>
+        Su compromiso solidario hace posible que ayudemos a tantos niños de la Argentina.
+      </p>
+    </div>
+
+    <div class="row no-gutters agrade-container">
+      <div class="col"><a class="social-inner" href="#"><img src="img/atilra.jpg" alt=""></a></div>
+      <div class="col"><a class="social-inner" href="#"><img src="img/cali_logo.jpg" alt=""></a></div>
+      <div class="col"><a class="social-inner" href="#"><img src="img/districar_logo.jpg" alt=""></a></div>
+      <div class="col"><a class="social-inner" href="#"><img src="img/fadepa_logo.jpg" alt=""></a></div>
+    </div>
+
+    <div class="row no-gutters agrade-container">
+      <div class="col"><a class="social-inner" href="#"><img src="img/iso_rosamonte.jpg" alt=""></a></div>
+      <div class="col"><a class="social-inner" href="#"><img src="img/LOGO CONTA.jpg" alt=""></a></div>
+      <div class="col"><a class="social-inner" href="#"><img src="img/logo_lospinos.jpg" alt=""></a></div>
+      <div class="col"><a class="social-inner" href="#"><img src="img/logo-epta.jpg" alt=""></a></div>
+    </div>
+  </section>
+
+
+
+      <footer class="section footer-classic context-dark bg-image bg-dark piepagina">
+          <div class="container">
+            <div class="row row-30">
+              <div class="col-md-4 col-xl-5">
+                <div class="pr-xl-4">
+                  <a class="brand" href="index.html"><img class="imagen-logo" src="img/logo_baja.png" alt="" width="140" height="37" srcset="img/logo_baja.png 2x"></a>
+                  <p> Trabajamos por l@s niños y niñas que se encuentran en zonas inhóspitas e inaccesibles de la cordillera Argentina. </p>
+                  <!-- Rights-->
+                  <p class="rights"><span>© </span><span class="copyright-year">2019</span><span> </span><span>Waves</span><span>. </span><span> Todos los derechos reservados</span></p>
+                </div>
+              </div>
+
+              <div class="col-md-4">
+                <h5>Contacto</h5>
+                <dl class="contact-list">
+                  <dt> Informes: </dt>
+                  <dd>de Lunes a Viernes de 8 a 17 hs. </dd>
+                </dl>
+                <dl class="contact-list">
+                  <dt>Email:</dt>
+                  <dd><a href="mailto:#">fnm.sedecentral@gmail.com </a></dd>
+                </dl>
+                <dl class="contact-list">
+                  <dt>Teléfono:</dt>
+                  <dd><a href="tel:#">+53 351 152532021</a> <span>or</span> <a href="tel:#">+53 351 152532022</a>
+                  </dd>
+                </dl>
+              </div>
+              <div class="col-md-4 col-xl-3">
+                <h5>Nuestro Sitio</h5>
+                <ul class="nav-list">
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Quienes Somos</a></li>
+                  <li><a href="#">Area de Distribución</a></li>
+                  <li><a href="#">Eventos</a></li>
+                  <li><a href="#">Agradecientos</a></li>
+                  <li><a href="#">Contacto</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div class="row no-gutters social-container">
+            <div class="col"><a class="social-inner" href="#"><span class="icon mdi mdi-facebook"></span><span>Facebook</span></a></div>
+            <div class="col"><a class="social-inner" href="#"><span class="icon mdi mdi-instagram"></span><span>instagram</span></a></div>
+            <div class="col"><a class="social-inner" href="#"><span class="icon mdi mdi-twitter"></span><span>twitter</span></a></div>
+            <div class="col"><a class="social-inner" href="#"><span class="icon mdi mdi-youtube-play"></span><span>google</span></a></div>
+          </div>
+        </footer>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
