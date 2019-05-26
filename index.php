38546ff53eb0051c6a8e4e7f7c33228418ff0730
solecardazzo <soledadcardazzo@gmail.com>
2019-05-26 15:56:54 -0300

    cambie portada

--- index.php
+++ index.php
@@ -15,14 +15,18 @@
               <div class="carousel-item active">
                 <img src="img/carousel01.jpg" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                  <button type="button" class="btn boton-dona">Doná</button>
-                  <h5>Sabemos que es posible</h5>
-                  <p>Si todos colaboramos con un poco</p>
+                  <img class="titulo-logo" src="img/logo_baja.png" alt="">
+                  <p>
+                    <button type="button" class="btn boton-dona">Doná</button>
+                    <h5 class="subtitulos-carousel">Sabemos que es posible</h5>
+                    <h5 class="subtitulos-carousel">Si todos colaboramos con un poco</h5>
+                  </p>
                 </div>
               </div>
               <div class="carousel-item">
                 <img src="img/carousel02.jpg" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
+                  <h1 class="titulo-carousel">Fundación los niños del mañana</h1>
                   <button type="button" class="btn boton-dona">Doná</button>
                   <h5>Sabemos que es posible</h5>
                   <p>Si todos colaboramos con un poco</p>
@@ -31,9 +35,12 @@
               <div class="carousel-item">
                 <img src="img/carousel03.jpg" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                  <button type="button" class="btn boton-dona">Doná</button>
-                  <h5>Sabemos que es posible</h5>
-                  <p>Si todos colaboramos con un poco</p>
+                  <img class="titulo-logo" src="img/logo_baja.png" alt="">
+                  <p>
+                    <button type="button" class="btn boton-dona">Doná</button>
+                    <h5 class="subtitulos-carousel">Sabemos que es posible</h5>
+                    <h5 class="subtitulos-carousel">Si todos colaboramos con un poco</h5>
+                  </p>
                 </div>
               </div>
             </div>
